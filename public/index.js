588a3fe1e44660e864247895ff23ef6f70ee4404
Lertos <deetasticgaming@gmail.com>
2022-09-15 22:47:36 -0300

    Refactoring

--- public/index.js
+++ public/index.js
@@ -142,72 +142,85 @@ function moveMap() {
     if (moveDir != '')
         newMovement = false
 
-    if (pressedKey == 'a' && moveDir == '') {
-            moveDir = 'left'
-    } else if (pressedKey == 'd' && moveDir == '') {
-            moveDir = 'right'
-    } else if (pressedKey == 's' && moveDir == '') {
-            moveDir = 'down'
-    } else if (pressedKey == 'w' && moveDir == '') {
-            moveDir = 'up'
-    }
+    setNewDirection()
 
-    if (moveDir != '') {
-        if (moveDir == 'left')
-            x = 1
-        else if (moveDir == 'right')
-            x = -1
-        else if (moveDir == 'down')
-            y = -1
-        else if (moveDir == 'up')
-            y = 1
-    }
+    let directionValues = getDirectionValue()
+
+    x = directionValues.x
+    y = directionValues.y
 
     //There is movement taking place
     if (x != 0 || y != 0) {
         if (newMovement)
             startingTile = { x: backgroundSprite.position.x, y: backgroundSprite.position.y }
 
+        let possibleTile = { x: currentTile.x - x , y: currentTile.y - y }
+
+        if (isBlocked(possibleTile)) {
+            moveDir = ''
+            return
+        }
+
+        destinationTile = { x: startingTile.x + (tileSize * x), y: startingTile.y + (tileSize * y) }
+
         if (x != 0) {
-            let possibleTile = { x: currentTile.x - x , y: currentTile.y }
-
-            if (isBlocked(possibleTile)) {
-                moveDir = ''
-                return
-            }
-
-            destinationTile = { x: startingTile.x + (tileSize * x), y: startingTile.y }
-
             if (Math.abs(destinationTile.x - backgroundSprite.position.x) < moveSpeed) {
-                backgroundSprite.position.x = destinationTile.x
-                foregroundSprite.position.x = destinationTile.x
+                setPositionOfMapLayers(destinationTile.x, 0)
                 moveDir = ''
                 currentTile.x -= x
             } else {
-                backgroundSprite.position.x += (moveSpeed * x)
-                foregroundSprite.position.x += (moveSpeed * x)
+                setPositionOfMapLayers(backgroundSprite.position.x + (moveSpeed * x), 0)
             }
         } else if (y != 0) {
-            let possibleTile = { x: currentTile.x, y: currentTile.y - y }
-
-            if (isBlocked(possibleTile)) {
-                moveDir = ''
-                return
-            }
-
-            destinationTile = { x: startingTile.x, y: startingTile.y + (tileSize * y) }
-
             if (Math.abs(destinationTile.y - backgroundSprite.position.y) < moveSpeed) {
-                backgroundSprite.position.y = destinationTile.y
-                foregroundSprite.position.y = destinationTile.y
+                setPositionOfMapLayers(0, destinationTile.y)
                 moveDir = ''
                 currentTile.y -= y
             } else {
-                backgroundSprite.position.y += (moveSpeed * y)
-                foregroundSprite.position.y += (moveSpeed * y)
+                setPositionOfMapLayers(0, backgroundSprite.position.y + (moveSpeed * y))
             }
         }
     }
+}
+
+
+function setPositionOfMapLayers(x, y) {
+    if (x != 0) {
+        backgroundSprite.position.x = x
+        foregroundSprite.position.x = x
+    } else if (y != 0) {
+        backgroundSprite.position.y = y
+        foregroundSprite.position.y = y
+    }
+}
+
+function setNewDirection() {
+    if (pressedKey == 'a' && moveDir == '') {
+        moveDir = 'left'
+    } else if (pressedKey == 'd' && moveDir == '') {
+        moveDir = 'right'
+    } else if (pressedKey == 's' && moveDir == '') {
+        moveDir = 'down'
+    } else if (pressedKey == 'w' && moveDir == '') {
+        moveDir = 'up'
+    }
+}
+
+function getDirectionValue() {
+    let directions = { x: 0, y: 0 }
+
+    if (moveDir != '') {
+        if (moveDir == 'left')
+            directions.x = 1
+        else if (moveDir == 'right')
+            directions.x = -1
+        else if (moveDir == 'down')
+            directions.y = -1
+        else if (moveDir == 'up')
+            directions.y = 1
+    }
+
+    return directions
 }
 
 function isBlocked(tile) {
