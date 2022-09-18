eed3d2ec49e7d7bb661f9aa56d42a0a3a0cfea60
Lertos <deetasticgaming@gmail.com>
2022-09-18 18:13:55 -0300

    Huge refactor; adding map class

--- public/classes.js
+++ public/classes.js
@@ -9,10 +9,8 @@ class Sprite {
         this.elapsedFrames = 0
         this.idleFrame = frames.currX
 
-        this.image.onload = () => {
-            this.width = this.image.width / this.frames.maxX
-            this.height = this.image.height / this.frames.maxY
-        }
+        this.width = this.image.width / this.frames.maxX
+        this.height = this.image.height / this.frames.maxY
 
         this.spriteRowForDirection = ['down', 'left', 'right', 'up']
 
@@ -88,6 +86,145 @@ class Sprite {
                 this.isFinishing = false
             }
         }
+    }
+
+}
+
+class InstanceMap {
+
+    constructor({ id, bgImage, fgImage, mapSize, collisions, battleZones }) {
+        this.id = id
+        this.mapSize = mapSize
+        this.collisions = collisions
+        this.battleZones = battleZones
+
+        this.bgImage = bgImage
+        this.fgImage = fgImage
+    
+        //Add any object that should be moved along with the map
+        this.movingSprites = [bgImage, fgImage]
+
+        this.mapCollisions = []
+
+        for (let i=0; i<this.collisions.length; i+=this.mapSize.x)
+            this.mapCollisions.push(this.collisions.slice(i, this.mapSize.x + i))
+
+        this.mapBattleZones = []
+
+        for (let i=0; i<this.battleZones.length; i+=this.mapSize.x)
+            this.mapBattleZones.push(this.battleZones.slice(i, this.mapSize.x + i))
+
+    }
+    
+    moveMap() {
+        let x = 0
+        let y = 0
+        let newMovement = true
+
+        //Track if this is a new movement or not
+        if (moveDir != '')
+            newMovement = false
+
+            this.setNewDirection()
+
+        let directionValues = this.getDirectionValue()
+
+        x = directionValues.x
+        y = directionValues.y
+
+        //There is movement taking place
+        if (x != 0 || y != 0) {
+            if (newMovement)
+                startingTile = { x: this.bgImage.position.x, y: this.bgImage.position.y }
+
+            let possibleTile = { x: currentTile.x - x , y: currentTile.y - y }
+
+            //Handle collision detection
+            if (this.isTileType(possibleTile, this.mapCollisions)) {
+                moveDir = ''
+                return
+            }
+
+            //Tile must be walkable. Proceed with the movement calculation
+            this.handleFrameMovement(x, y)
+        }
+    }
+
+    handleFrameMovement(x, y) {
+        destinationTile = { x: startingTile.x + (tileSize * x), y: startingTile.y + (tileSize * y) }
+
+        let xMovement = Math.abs(destinationTile.x - this.bgImage.position.x)
+        let yMovement = Math.abs(destinationTile.y - this.bgImage.position.y)
+
+        //If the distance to travel is less then the movement speed, set them to the destination
+        if ((xMovement != 0 && xMovement < moveSpeed) || (yMovement != 0 && yMovement < moveSpeed)) {
+            xMovement = destinationTile.x
+            yMovement = destinationTile.y
+
+            this.onDestinationArrival(x, y)
+        } else {
+            xMovement = this.bgImage.position.x + (moveSpeed * x)
+            yMovement = this.bgImage.position.y + (moveSpeed * y)
+        }
+
+        this.setPositionOfMapLayers(xMovement, yMovement)
+    }
+
+    onDestinationArrival(x, y) {
+        currentTile.x -= x
+        currentTile.y -= y
+
+        moveDir = ''
+
+        if (this.isTileType(currentTile, this.mapBattleZones)) {
+            //TODO: This should come from a map default such as "battle rate"
+            if (Math.random() < 0.25)
+                console.log('battle')
+        }
+    }
+
+    setPositionOfMapLayers(x, y) {
+        for (let i = 0; i < this.movingSprites.length; i++) {
+            this.movingSprites[i].position.x = x
+            this.movingSprites[i].position.y = y
+        }
+    }
+
+    isTileType(tile, tileType) {
+        var tileValue = tileType[tile.y][tile.x]
+    
+        if (tileValue == 0)
+            return false
+        return true
+    }
+
+    setNewDirection() {
+        if (pressedKey == 'a' && moveDir == '') {
+            moveDir = 'left'
+        } else if (pressedKey == 'd' && moveDir == '') {
+            moveDir = 'right'
+        } else if (pressedKey == 's' && moveDir == '') {
+            moveDir = 'down'
+        } else if (pressedKey == 'w' && moveDir == '') {
+            moveDir = 'up'
+        }
+    }
+    
+    getDirectionValue() {
+        let directions = { x: 0, y: 0 }
+    
+        if (moveDir != '') {
+            if (moveDir == 'left')
+                directions.x = 1
+            else if (moveDir == 'right')
+                directions.x = -1
+            else if (moveDir == 'down')
+                directions.y = -1
+            else if (moveDir == 'up')
+                directions.y = 1
+        }
+    
+        return directions
     }
 
 }
--- public/index.js
+++ public/index.js
@@ -4,109 +4,119 @@ const ctx = canvas.getContext('2d')
 canvas.width = 1024
 canvas.height = 576
 
+const tileSize = 16
 const ctxScale = {x: 3, y: 3}
-
-const moveSpeed = 1.2
 
 ctx.scale(ctxScale.x, ctxScale.y)
 ctx.imageSmoothingEnabled = false
 
-const tileSize = 16
-//TODO: This should come from the map; when logging in, entering a new map, etc
-const startTile = {x: 13, y: 19}
-let currentTile = startTile
+//TODO: Move to player class
+const moveSpeed = 1.2
 
-const mapCollisions = []
-
-//TODO: Change the i+ value to the amount of horz tiles from the Map object 
-for (let i=0; i<colHomeIsland.length; i+=40) {
-    mapCollisions.push(colHomeIsland.slice(i, 40 + i))
+//Load all images
+const imagePaths = {
+    bg: './img/maps/images/home_island.png',
+    fg: './img/maps/images/home_island_fg.png',
+    player: './img/characters/human1.png'
 }
 
-const mapBattleZones = []
+let loadedCounter = 0
 
-//TODO: Change the i+ value to the amount of horz tiles from the Map object 
-for (let i=0; i<zonesHomeIsland.length; i+=40) {
-    mapBattleZones.push(zonesHomeIsland.slice(i, 40 + i))
+for (const [key, value] of Object.entries(imagePaths)) {
+    const img = new Image()
+    img.src = value
+
+    img.onload = () => {
+        imagePaths[key] = img
+        loadedCounter++
+
+        if (loadedCounter == Object.keys(imagePaths).length)
+            startGame()
+    }
 }
 
+let currentMap
+let playerSprite
 
-const startPixel = {
+let startTile = { x: 13, y: 19 }
+
+let startPixel = {
     x: (startTile.x * tileSize * -1),
     y: (startTile.y * tileSize * -1)
 }
 
-const bgImage = new Image()
-bgImage.src = './img/maps/home_island.png'
+//TODO: This is the players current tile
+let currentTile = startTile
 
-const fgImage = new Image()
-fgImage.src = './img/maps/home_island_fg.png'
+let pressedKey = ''
+let moveDir = ''
+let startingTile = { x: 0, y: 0}
+let destinationTile = { x: 0, y: 0}
 
-const playerImage = new Image()
-playerImage.src = './img/characters/human2.png'
+//For the animation logic
+let fps, fpsInterval, startTime, now, then, elapsed
+
+function startGame() {
+    let bgImage = new Sprite({
+        position: {
+            x: roundToTileSize(startPixel.x + (canvas.width / 2) / ctxScale.x),
+            y: roundToTileSize(startPixel.y + (canvas.height / 2) / ctxScale.y)
+        },
+        image: imagePaths.bg,
+        offset: { x: 0, y: 0 },
+        frames: {
+            maxX: 1,
+            maxY: 1,
+            currX: 0,
+            currY: 0,
+        }
+    })
+
+    let fgImage = new Sprite({
+        position: {
+            x: roundToTileSize(startPixel.x + (canvas.width / 2) / ctxScale.x),
+            y: roundToTileSize(startPixel.y + (canvas.height / 2) / ctxScale.y)
+        },
+        image: imagePaths.fg,
+        offset: { x: 0, y: 0 },
+        frames: {
+            maxX: 1,
+            maxY: 1,
+            currX: 0,
+            currY: 0,
+        }
+    })
+
+    currentMap = new InstanceMap({
+        id: 'homeIsland',
+        bgImage: bgImage,
+        fgImage: fgImage,
+        mapSize: { x: 40, y: 30 },
+        collisions: colHomeIsland,
+        battleZones: zonesHomeIsland
+    })
+
+    playerSprite = new Sprite({
+        position: {
+            x: roundToTileSize((canvas.width / 2) / ctxScale.x),
+            y: roundToTileSize((canvas.height / 2) / ctxScale.y)
+        },
+        image: imagePaths.player,
+        offset: { x: 0, y: -4 },
+        frames: {
+            maxX: 3,
+            maxY: 4,
+            currX: 1,
+            currY: 0,
+        }
+    })
+
+    startAnimating(60)
+}
 
 function roundToTileSize(num) {
     return Math.ceil(num / tileSize) * tileSize;
 }
-
-const backgroundSprite = new Sprite({
-    position: {
-        x: roundToTileSize(startPixel.x + (canvas.width / 2) / ctxScale.x),
-        y: roundToTileSize(startPixel.y + (canvas.height / 2) / ctxScale.y)
-    },
-    image: bgImage,
-    offset: { x: 0, y: 0 },
-    frames: {
-        maxX: 1,
-        maxY: 1,
-        currX: 0,
-        currY: 0,
-    }
-})
-
-const foregroundSprite = new Sprite({
-    position: {
-        x: roundToTileSize(startPixel.x + (canvas.width / 2) / ctxScale.x),
-        y: roundToTileSize(startPixel.y + (canvas.height / 2) / ctxScale.y)
-    },
-    image: fgImage,
-    offset: { x: 0, y: 0 },
-    frames: {
-        maxX: 1,
-        maxY: 1,
-        currX: 0,
-        currY: 0,
-    }
-})
-
-const playerSprite = new Sprite({
-    position: {
-        x: roundToTileSize((canvas.width / 2) / ctxScale.x),
-        y: roundToTileSize((canvas.height / 2) / ctxScale.y)
-    },
-    image: playerImage,
-    offset: { x: 0, y: -4 },
-    frames: {
-        maxX: 3,
-        maxY: 4,
-        currX: 1,
-        currY: 0,
-    }
-})
-
-//Add any object that should be moved along with the map
-let movingSprites = [backgroundSprite, foregroundSprite]
-
-let pressedKey = ''
-let startingTile = { x: 0, y: 0}
-let destinationTile = { x: 0, y: 0}
-let moveDir = ''
-
-//Animation
-
-let fps, fpsInterval, startTime, now, then, elapsed
-
-startAnimating(60)
 
 function startAnimating(fps) {
     fpsInterval = 1000 / fps
@@ -126,125 +136,16 @@ function animate(newTime) {
     if (elapsed > fpsInterval) {
         then = now - (elapsed % fpsInterval)
         
-        backgroundSprite.draw()
+        currentMap.bgImage.draw()
         playerSprite.draw()
-        foregroundSprite.draw()
+        currentMap.fgImage.draw()
 
         playerSprite.drawName()
 
-        moveMap()
+        currentMap.moveMap()
 
         playerSprite.moving = moveDir
     }
-}
-
-animate()
-
-function moveMap() {
-    let x = 0
-    let y = 0
-    let newMovement = true
-
-    //Track if this is a new movement or not
-    if (moveDir != '')
-        newMovement = false
-
-    setNewDirection()
-
-    let directionValues = getDirectionValue()
-
-    x = directionValues.x
-    y = directionValues.y
-
-    //There is movement taking place
-    if (x != 0 || y != 0) {
-        if (newMovement)
-            startingTile = { x: backgroundSprite.position.x, y: backgroundSprite.position.y }
-
-        let possibleTile = { x: currentTile.x - x , y: currentTile.y - y }
-
-        //Handle collision detection
-        if (isTileType(possibleTile, mapCollisions)) {
-            moveDir = ''
-            return
-        }
-
-        //Tile must be walkable. Set the destination tile
-        destinationTile = { x: startingTile.x + (tileSize * x), y: startingTile.y + (tileSize * y) }
-
-        let xMovement = Math.abs(destinationTile.x - backgroundSprite.position.x)
-        let yMovement = Math.abs(destinationTile.y - backgroundSprite.position.y)
-
-        //If the distance to travel is less then the movement speed, set them to the destination
-        if ((xMovement != 0 && xMovement < moveSpeed) || (yMovement != 0 && yMovement < moveSpeed)) {
-            xMovement = destinationTile.x
-            yMovement = destinationTile.y
-
-            onDestinationArrival(x, y)
-        } else {
-            xMovement = backgroundSprite.position.x + (moveSpeed * x)
-            yMovement = backgroundSprite.position.y + (moveSpeed * y)
-        }
-
-        setPositionOfMapLayers(xMovement, yMovement)
-    }
-}
-
-function onDestinationArrival(x, y) {
-    currentTile.x -= x
-    currentTile.y -= y
-
-    moveDir = ''
-
-    if (isTileType(currentTile, mapBattleZones)) {
-        //TODO: Do some random number roll to see if a battle will occur
-        if (Math.random() < 0.25)
-            console.log('battle')
-    }
-}
-
-function setPositionOfMapLayers(x, y) {
-    for (i = 0; i < movingSprites.length; i++) {
-        movingSprites[i].position.x = x
-        movingSprites[i].position.y = y
-    }
-}
-
-function setNewDirection() {
-    if (pressedKey == 'a' && moveDir == '') {
-        moveDir = 'left'
-    } else if (pressedKey == 'd' && moveDir == '') {
-        moveDir = 'right'
-    } else if (pressedKey == 's' && moveDir == '') {
-        moveDir = 'down'
-    } else if (pressedKey == 'w' && moveDir == '') {
-        moveDir = 'up'
-    }
-}
-
-function getDirectionValue() {
-    let directions = { x: 0, y: 0 }
-
-    if (moveDir != '') {
-        if (moveDir == 'left')
-            directions.x = 1
-        else if (moveDir == 'right')
-            directions.x = -1
-        else if (moveDir == 'down')
-            directions.y = -1
-        else if (moveDir == 'up')
-            directions.y = 1
-    }
-
-    return directions
-}
-
-function isTileType(tile, tileType) {
-    var tileValue = tileType[tile.y][tile.x]
-
-    if (tileValue == 0)
-        return false
-    return true
 }
 
 window.addEventListener('keydown', (e) => {
